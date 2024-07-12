33a4afe3d848673b6f576c64d8a182ff6d0f9eb2
Mat Marquis <mat@matmarquis.com>
2024-07-12 14:48:47 -0400

    Initial

---
+++ .eleventy.js
@@ -0,0 +1,43 @@
+const { DateTime } = require("luxon");
+const markdownIt = require("markdown-it");
+const sass = require('sass');
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("_src/img");
+
+    eleventyConfig.addFilter("formatDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toISODate();
+    });
+
+    eleventyConfig.addFilter(
+        'cssmin',
+        code => sass.compileString( code, {style: "compressed"}).css
+    );
+
+    let markdownOptions = {
+        html: true,
+        breaks: true,
+        linkify: true
+    };
+    let markdownLib = new markdownIt(markdownOptions);
+
+    eleventyConfig.setLibrary("md", markdownLib);
+
+    return {
+        templateFormats: [
+            "md"
+        ],
+        pathPrefix: "/",
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        passthroughFileCopy: true,
+        dir: {
+            input: "_src",
+            includes: "_templates",
+            data: "_data",
+            output: "_site"
+        }
+    };
+};
